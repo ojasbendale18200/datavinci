d41247e07774b2aeb8d198a0163f332a1ebb2c19
Ojas <ojasbendale18200@gmail.com>
2023-06-16 19:46:02 +0530

    d

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AllRoutes from "./Pages/AllRoutes";
+
+function App() {
+  return (
+    <>
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/Pages/AllRoutes.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+
+function AllRoutes() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default AllRoutes;
